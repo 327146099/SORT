420287c74bee8cab124c62fdca530f2290bdf84e
yangzhou <327146099@qq.com>
2022-01-25 17:22:52 +0800

    修改使用说明

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # SORT
+
+多目标跟踪 java 实现, 使用卡尔曼滤波器进行位置预测
+
+## 使用方式
+
+```java
+Sort sort = new Sort();
+List<Location> detections = new ArrayList<>();
+List<TrackResult> update = sort.update(detections);
+```
+detections为检测到的目标信息,不管是否检测到目标,每帧都需要调用`sort.update()`方法
+
+方法中的卡尔曼滤波器调用JKalman实现,使用时注意引入`JKalman.jar`
+
+[参考引用](https://blog.csdn.net/qq_40608730/article/details/118710715)
--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -2,7 +2,7 @@ import com.polaris.sort.Sort;
 import com.polaris.sort.entity.Location;
 import com.polaris.sort.entity.TrackResult;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SortTest {
@@ -10,9 +10,10 @@ public class SortTest {
         Sort sort = new Sort();
         int x = 10, y = 10;
         for (int i = 0; i < 100; i++) {
-            List<TrackResult> update = sort.update(Arrays.asList(new Location(x + i, y + i, x + 10 + i, y + i + 10, 1)));
+            List<Location> detections = new ArrayList<>();
+            detections.add(new Location(x + i, y + i, x + 10 + i, y + i + 10, 1));
+            List<TrackResult> update = sort.update(detections);
             System.out.println(update);
         }
-
     }
 }
